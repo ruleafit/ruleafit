29ed7d4d6810ae92047b91574a67795b161adef8
openfit2026 <openfit2026@gmail.com>
2026-08-02 09:22:33 +0200

    fix: lang=es en el html (la app está en español)

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
